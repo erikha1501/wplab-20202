81a06e7f6f3f5cf4bffa78f52b8e50f8cbe6f941
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-03-24 21:57:27 +0700

    Edit lab04

--- LAB_04/DriveDistance.php
+++ LAB_04/DriveDistance.php
@@ -1,13 +1,14 @@
 <html>
     <head>
-        <title>Distance form Chicago</title>
+        <title>Distance from Chicago!</title>
     </head>
     <body>
-        <font size="4 color=blue">Welcome to Distance Calculator!</font>
-        <br>The page that calculate distance
-        <br>Select a destination
-        <form action="CheckDistance.php" method="get">
+        <font size="4" color="blue"> Welcome to the Distance Calculator!"</font>
+        <br> The page that calculates the distances from Chicago.
+        <br> Select a destination:
+        <form action="CheckDistance.php" method="post">
             <select name="destination[]" size="5" multiple>
+                <option>Beijing</option>
                 <option>Boston</option>
                 <option>Dallas</option>
                 <option>Miami</option>
@@ -17,11 +18,10 @@
                 <option>San Francisco</option>
                 <option>Toronto</option>
                 <option>Washington, DC</option>
-                <option>Lol</option>
             </select>
             <br>
-            <input type="submit" value="submit">
-            <input type="reset" value="reset">
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
         </form>
     </body>
 </html>
